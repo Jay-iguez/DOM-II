118cfd5113c73555e332162ff72426861f4ebe62
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-06-01 21:23:01 -0700

    Create Click Element for H1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './less/index.less'
 
 // Your code goes here!
+function randomColorGen (num) {
+return `rgb(${Math.floor(num * Math.random())}, ${Math.floor(num * Math.random())}, ${Math.floor(num * Math.random())})`
+}
+
+
+
+
+const mainHeading = document.querySelector(".main-navigation h1")
+
+mainHeading.addEventListener("click", event => {
+    mainHeading.style.color = randomColorGen(255)
+    console.log(mainHeading)
+})
+
+const copyThanks = document.createElement("p")
+mainHeading.appendChild(copyThanks)
+
+const allElements = document.querySelector("*")
+
+allElements.addEventListener("copy", event => {
+copyThanks.textContent = `Thanks for copying a ${event.type.target}`
+console.log('blue cheese')
+})
+
+
+
